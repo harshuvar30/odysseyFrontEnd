6c981fbb5e21c5b242e601ca7c5526db54580492
Harshvardhan <hv9796923@gmail.com>
2024-01-17 15:47:02 +0530

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -22,8 +22,8 @@ const Products = ({cat,filters,sort}) => {
         try{
             const res = await axios.get(
             cat 
-            ? `http://localhost:5000/api/products?category=${cat}`
-            :"http://localhost:5000/api/products")
+            ? `https://odysseybackend-ze06.onrender.com/api/products?category=${cat}`
+            :"https://odysseybackend-ze06.onrender.com/api/products")
             setProducts(res.data);
 
             console.log(res);
